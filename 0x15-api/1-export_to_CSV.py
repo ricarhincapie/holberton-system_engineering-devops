485ceccd3e917f571bee63d9ccb31f0d90d87d32
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-15 13:29:00 +0000

    Add task #1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Module to fetch data from
+https://jsonplaceholder.typicode.com/ API"""
+
+
+if __name__ == '__main__':
+
+    import csv
+    import requests
+    import sys
+
+    empid = sys.argv[1]
+    apiurl = "https://jsonplaceholder.typicode.com/"
+
+    if int(empid):
+        payload = {'userId': empid}
+        response = requests.get(apiurl + "todos", params=payload)
+        json_response = response.json()
+        # All user info fetching
+        response_usr = requests.get(apiurl + "users")
+        usr_json_response = response_usr.json()
+        # Brings user name
+        for user in usr_json_response:
+            if user.get('id') == int(empid):
+                name = user.get('username')
+        # Creates csv file and gets the writer on
+        file_title = empid + ".csv"
+        csv_file = open(file_title, 'w')
+        header = ['USER_ID', 'USERNAME', 'TASK_COMPLETED', 'TASK_TITLE']
+        csv_writer = csv.DictWriter(csv_file, fieldnames=header)
+
+        # Orders data into a dictionary to pass it to csv writter
+        my_dict = {"USER_ID": {}, "USERNAME": {},
+                   "TASK_COMPLETED": {}, "TASK_TITLE": {}}
+        tmp = {}
+        print(json_response)
+        count = 0
+        for task in json_response:
+            my_dict.get('USER_ID').update({count: empid})
+            my_dict.get('USERNAME').update({count: name})
+            my_dict.get('TASK_COMPLETED').update({count:
+                                                  task.get('completed')})
+            my_dict.get('TASK_TITLE').update({count: task.get('title')})
+            count += 1
+        # Writes dictionary data in csv format
+        goal = 0
+        while goal < count:
+            tmp1 = my_dict.get('TASK_COMPLETED').get(goal)
+            tmp2 = my_dict.get('TASK_TITLE').get(goal)
+            csv_writer.writerow({'USER_ID': my_dict.get('USER_ID').get(goal),
+                                'USERNAME': my_dict.get('USERNAME').get(goal),
+                                'TASK_COMPLETED': tmp1,
+                                'TASK_TITLE': tmp2})
+            goal += 1
