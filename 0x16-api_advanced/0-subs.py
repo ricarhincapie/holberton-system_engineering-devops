12666431bb03f6ecc524ca25badd530671d0971b
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-17 17:16:54 +0000

    Add first task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module to consume Reddit API and
+get the number of suscribers to given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Takes a subreddir and makes a GET requests
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) \
+                              AppleWebKit/537.36 (KHTML, like Gecko) Chrome/\
+                              85.0.4183.102 Safari/537.36"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response:
+        json_response = response.json()
+        return(json_response.get('data').get('subscribers'))
+    else:
+        return 0
