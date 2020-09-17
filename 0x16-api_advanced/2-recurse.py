52e374ebc68dfc2e971a8c28b745959403ddad02
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-17 22:12:56 +0000

    Add task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Request the top ten hot posts
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Gets recursively the top hot posts in Reddit API
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    query = {'after': after, 'limit': 2}
+    response = requests.get(url,
+                            allow_redirects=False,
+                            params=query,
+                            headers={'User-Agent': 'MyMachintosh'})
+    if response and response.status_code == 200:
+        after = response.json().get('data').get('after')
+        if (after is None or len(hot_list) == 10):
+            return hot_list
+        post_list = response.json().get('data').get('children')
+        for children in post_list:
+            hot_list.append(children.get('data').get('title'))
+        return recurse(subreddit, hot_list, after)
+    else:
+        return None
